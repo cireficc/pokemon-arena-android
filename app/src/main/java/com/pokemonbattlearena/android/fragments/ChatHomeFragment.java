8e56939101201a11336fbe440065ceb3c2bdc7ea
Spencer Amann <amanns@mail.gvsu.edu>
2016-10-01 10:17:06 -0400

    Back to the basics

--- app/src/main/java/com/pokemonbattlearena/android/fragments/ChatHomeFragment.java
+++ app/src/main/java/com/pokemonbattlearena/android/fragments/ChatHomeFragment.java
@@ -22,6 +22,7 @@ public class ChatHomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_chathome, container, false);
+        View view = inflater.inflate(R.layout.fragment_chathome, container, false);
+        return view;
     }
 }
