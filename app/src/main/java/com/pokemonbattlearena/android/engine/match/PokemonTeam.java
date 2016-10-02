de100254bab3504633f3500f04343f3abab6eb68
Chris Cirefice <cireficc@mail.gvsu.edu>
2016-10-02 18:03:32 -0400

    Add Pokemon 1-by-1 to the team

--- app/src/main/java/com/pokemonbattlearena/android/engine/match/PokemonTeam.java
+++ app/src/main/java/com/pokemonbattlearena/android/engine/match/PokemonTeam.java
@@ -2,18 +2,30 @@ package com.pokemonbattlearena.android.engine.match;
 
 import com.pokemonbattlearena.android.engine.database.Pokemon;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PokemonTeam {
 
+    private int size;
+
     private List<Pokemon> pokemons;
 
-    public PokemonTeam(List<Pokemon> pokemons) {
-        this.pokemons = pokemons;
+    public PokemonTeam(int size) {
+        this.size = size;
+        this.pokemons = new ArrayList<>();
     }
 
     public List<Pokemon> getPokemons() {
         return pokemons;
+    }
+
+    public void addPokemon(Pokemon p) {
+        pokemons.add(p);
+    }
+
+    public void removePokemon(Pokemon p) {
+        pokemons.remove(p);
     }
 
     public Pokemon currentPokemon() {
