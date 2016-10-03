e4e0623a27809d067ac12deb97cb9bbc51193bb3
Spencer Amann <amanns@mail.gvsu.edu>
2016-10-03 16:24:24 -0400

    Add application

--- app/src/main/java/com/pokemonbattlearena/android/fragments/BattleHomeFragment.java
+++ app/src/main/java/com/pokemonbattlearena/android/fragments/BattleHomeFragment.java
@@ -14,11 +14,7 @@ import com.pokemonbattlearena.android.R;
  * Created by droidowl on 9/25/16.
  */
 
-public class BattleHomeFragment extends Fragment implements View.OnClickListener {
-
-    private Button mBattleButton;
-    private boolean battleBegun = false;
-    private BattleUIFragment battleUIFragment;
+public class BattleHomeFragment extends Fragment {
 
     public BattleHomeFragment() {
         super();
@@ -28,23 +24,6 @@ public class BattleHomeFragment extends Fragment implements View.OnClickListener
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_battlehome, container, false);
-        mBattleButton = (Button) view.findViewById(R.id.quick_battle_button);
-        mBattleButton.setOnClickListener(this);
         return view;
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (!battleBegun) {
-            battleBegun = true;
-            battleUIFragment = new BattleUIFragment();
-            getFragmentManager().beginTransaction().add(R.id.battle_ui_container, battleUIFragment).commit();
-            mBattleButton.setText(R.string.cancel_battle);
-        } else {
-            getFragmentManager().beginTransaction().remove(battleUIFragment).commit();
-            battleUIFragment = null;
-            mBattleButton.setText(R.string.battle);
-            battleBegun = false;
-        }
     }
 }
