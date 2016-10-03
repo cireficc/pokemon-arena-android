33d2682fae1c352404bfbfaf0541480e2c5e8826
Nathan Anderle <anderle.nathan8@gmail.com>
2016-10-03 18:00:57 -0400

    Add  BattlePokemon moveSet

--- app/src/main/java/com/pokemonbattlearena/android/engine/match/BattlePokemon.java
+++ app/src/main/java/com/pokemonbattlearena/android/engine/match/BattlePokemon.java
@@ -1,11 +1,13 @@
 package com.pokemonbattlearena.android.engine.match;
 
+import com.pokemonbattlearena.android.engine.database.Move;
 import com.pokemonbattlearena.android.engine.database.Pokemon;
 
 public class BattlePokemon {
 
     private Pokemon originalPokemon;
     private int currentHp;
+    private Move[] moveSet = new Move[4];
 
     public BattlePokemon(Pokemon pokemon) {
 
@@ -20,4 +22,8 @@ public class BattlePokemon {
     public void setCurrentHp(int currentHp) {
         this.currentHp = currentHp;
     }
+
+    public Move[] getMoveSet() { return moveSet; }
+
+    public void setMoveSet(Move[] chosenMoves) { this.moveSet = chosenMoves; }
 }
