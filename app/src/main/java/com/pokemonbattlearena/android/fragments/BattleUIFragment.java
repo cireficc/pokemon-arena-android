a879bad5d3fbd1aa9369f246ce1f1fb580a726ad
Spencer Amann <amanns@mail.gvsu.edu>
2016-10-05 17:43:57 -0400

    Connects to a room

--- app/src/main/java/com/pokemonbattlearena/android/fragments/BattleUIFragment.java
+++ app/src/main/java/com/pokemonbattlearena/android/fragments/BattleUIFragment.java
@@ -1,7 +1,9 @@
 package com.pokemonbattlearena.android.fragments;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -9,17 +11,27 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.games.Games;
+import com.google.android.gms.games.multiplayer.Multiplayer;
+import com.google.android.gms.games.multiplayer.Participant;
+import com.google.android.gms.games.multiplayer.realtime.RealTimeMessage;
+import com.google.android.gms.games.multiplayer.realtime.RealTimeMessageReceivedListener;
+import com.google.android.gms.games.multiplayer.realtime.Room;
+import com.google.android.gms.games.multiplayer.realtime.RoomConfig;
+import com.google.android.gms.games.multiplayer.realtime.RoomUpdateListener;
 import com.pokemonbattlearena.android.PokemonBattleApplication;
 import com.pokemonbattlearena.android.R;
 import com.pokemonbattlearena.android.TypeModel;
 import com.pokemonbattlearena.android.engine.database.Move;
 import com.pokemonbattlearena.android.engine.database.Pokemon;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -27,7 +39,58 @@ import java.util.Random;
  * Created by Spencer Amann on 10/1/16.
  */
 
-public class BattleUIFragment extends Fragment implements View.OnClickListener {
+public class BattleUIFragment extends Fragment implements View.OnClickListener, RealTimeMessageReceivedListener, RoomUpdateListener  {
+
+    static final String TAG = "Pokemon Battle Room";
+    String mRoomId = null;
+
+    // Are we playing in multiplayer mode?
+    boolean mMultiplayer = false;
+
+    // The participants in the currently active game
+    ArrayList<Participant> mParticipants = null;
+
+    // My participant ID in the currently active game
+    String mMyId = null;
+
+    // If non-null, this is the id of the invitation we received via the
+    // invitation listener
+    String mIncomingInvitationId = null;
+
+    // Message buffer for sending messages
+    byte[] mMsgBuf = new byte[2];
+
+    //TODO: Start RoomUpdateListener
+    @Override
+    public void onRoomCreated(int i, Room room) {
+
+    }
+
+    @Override
+    public void onJoinedRoom(int i, Room room) {
+
+        Log.e("Pokemon Battle Room", "Joined");
+        mRoom = room;
+    }
+
+    @Override
+    public void onLeftRoom(int i, String s) {
+
+    }
+
+    @Override
+    public void onRoomConnected(int i, Room room) {
+
+        Log.e("Pokemon Battle Room", "Connected");
+        //get participants and my ID:
+        mParticipants = room.getParticipants();
+        mMyId = room.getParticipantId(Games.Players.getCurrentPlayerId(mApplication.getGoogleApiClient()));
+
+        // save room ID if its not initialized in onRoomCreated() so we can leave cleanly before the game starts.
+        if(mRoomId==null)
+            mRoomId = room.getRoomId();
+    }
+
 
     PokemonBattleApplication mApplication = PokemonBattleApplication.getInstance();
 
@@ -38,6 +101,13 @@ public class BattleUIFragment extends Fragment implements View.OnClickListener {
     private List<Move> mPlayerMoves;
 
     private Button[] mMoveButtons;
+
+    private Room mRoom;
+
+    final static int RC_SELECT_PLAYERS = 10000;
+
+    // launch the player selection screen
+// minimum: 1 other player; maximum: 3 other players
 
     public BattleUIFragment() {
         super();
@@ -98,6 +168,7 @@ public class BattleUIFragment extends Fragment implements View.OnClickListener {
         if (mPlayerPokemon != null) {
             switch (v.getId()) {
                 case R.id.move_button_0:
+                    startQuickGame();
                     Toast.makeText(mApplication, mPlayerMoves.get(0).getName() + ": " + mPlayerMoves.get(0).getPower(), Toast.LENGTH_SHORT).show();
                     break;
                 case R.id.move_button_1:
@@ -133,5 +204,90 @@ public class BattleUIFragment extends Fragment implements View.OnClickListener {
         String key = "ic_pokemon_" + name.toLowerCase();
         int id = getResources().getIdentifier(key, "drawable", c.getPackageName());
         return c.getDrawable(id);
+    }
+
+    private void startQuickGame() {
+        // auto-match criteria to invite one random automatch opponent.
+        // You can also specify more opponents (up to 3).
+        Bundle am = RoomConfig.createAutoMatchCriteria(1, 1, 0);
+
+        // build the room config:
+        RoomConfig.Builder roomConfigBuilder = makeBasicRoomConfigBuilder();
+        roomConfigBuilder.setAutoMatchCriteria(am);
+        RoomConfig roomConfig = roomConfigBuilder.build();
+
+        // create room:
+        Games.RealTimeMultiplayer.create(mApplication.getGoogleApiClient(), roomConfig);
+
+        // prevent screen from sleeping during handshake
+        getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        // go to game screen
+        Intent intent = Games.RealTimeMultiplayer.getSelectOpponentsIntent(mApplication.getGoogleApiClient(), 1, 3);
+        startActivityForResult(intent, RC_SELECT_PLAYERS);
+    }
+
+    // create a RoomConfigBuilder that's appropriate for your implementation
+    private RoomConfig.Builder makeBasicRoomConfigBuilder() {
+        return RoomConfig.builder(this)
+                .setMessageReceivedListener(this);
+    }
+
+    @Override
+    public void onActivityResult(int request, int response, Intent data) {
+        if (request == RC_SELECT_PLAYERS) {
+            if (response != Activity.RESULT_OK) {
+                // user canceled
+                return;
+            }
+
+            // get the invitee list
+            Bundle extras = data.getExtras();
+            final ArrayList<String> invitees =
+                    data.getStringArrayListExtra(Games.EXTRA_PLAYER_IDS);
+
+            // get auto-match criteria
+            Bundle autoMatchCriteria = null;
+            int minAutoMatchPlayers =
+                    data.getIntExtra(Multiplayer.EXTRA_MIN_AUTOMATCH_PLAYERS, 0);
+            int maxAutoMatchPlayers =
+                    data.getIntExtra(Multiplayer.EXTRA_MAX_AUTOMATCH_PLAYERS, 0);
+
+            if (minAutoMatchPlayers > 0) {
+                autoMatchCriteria = RoomConfig.createAutoMatchCriteria(
+                        minAutoMatchPlayers, maxAutoMatchPlayers, 0);
+            } else {
+                autoMatchCriteria = null;
+            }
+
+            // create the room and specify a variant if appropriate
+            RoomConfig.Builder roomConfigBuilder = makeBasicRoomConfigBuilder();
+            roomConfigBuilder.addPlayersToInvite(invitees);
+            if (autoMatchCriteria != null) {
+                roomConfigBuilder.setAutoMatchCriteria(autoMatchCriteria);
+            }
+            RoomConfig roomConfig = roomConfigBuilder.build();
+            Games.RealTimeMultiplayer.create(mApplication.getGoogleApiClient(), roomConfig);
+
+            // prevent screen from sleeping during handshake
+            getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+    }
+
+    private void sendMessage(Pokemon p1, Pokemon p2) {
+        byte[] message = p1.toString().getBytes();
+        for (Participant p : mParticipants) {
+            if (!p.getParticipantId().equals(mMyId)) {
+                Games.RealTimeMultiplayer.sendReliableMessage(mApplication.getGoogleApiClient(), null, message,
+                        mRoomId, p.getParticipantId());
+            }
+        }
+    }
+
+    @Override
+    public void onRealTimeMessageReceived(RealTimeMessage realTimeMessage) {
+        byte[] buf = realTimeMessage.getMessageData();
+        String sender = realTimeMessage.getSenderParticipantId();
+        Log.d(TAG, "Message received: " + (char) buf[0] + "/" + (int) buf[1]);
     }
 }
