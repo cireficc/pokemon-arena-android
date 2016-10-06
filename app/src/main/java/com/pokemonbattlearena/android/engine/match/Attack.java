3d8c5b248c3821dc45538a86cd9ad1cf372f84cb
Chris Cirefice <cireficc@mail.gvsu.edu>
2016-10-05 21:14:20 -0400

    Rename receiver to target

--- app/src/main/java/com/pokemonbattlearena/android/engine/match/Attack.java
+++ app/src/main/java/com/pokemonbattlearena/android/engine/match/Attack.java
@@ -6,14 +6,14 @@ public class Attack implements Command {
 
     private Move move;
     private BattlePokemon attacker;
-    private BattlePokemon receiver;
+    private BattlePokemon target;
 
     private static DamageCalculator damageCalculator = DamageCalculator.getInstance();
 
-    public Attack(BattlePokemon attacker, Move move, BattlePokemon receiver) {
+    public Attack(BattlePokemon attacker, Move move, BattlePokemon target) {
         this.move = move;
         this.attacker = attacker;
-        this.receiver = receiver;
+        this.target = target;
     }
 
     @Override
@@ -21,7 +21,7 @@ public class Attack implements Command {
 
         // TODO: Actually use real damage/effect calculations
         int damage = move.getPower();
-        int remainingHp = receiver.getCurrentHp() - damage;
-        receiver.setCurrentHp(remainingHp);
+        int remainingHp = target.getCurrentHp() - damage;
+        target.setCurrentHp(remainingHp);
     }
 }
