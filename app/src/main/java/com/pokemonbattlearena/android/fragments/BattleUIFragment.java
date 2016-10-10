b7a27c3cd08b19228b6b5818b8211aebaaef468b
Spencer Amann <amanns@mail.gvsu.edu>
2016-10-09 23:26:30 -0400

    Send and parse name

--- app/src/main/java/com/pokemonbattlearena/android/fragments/BattleUIFragment.java
+++ app/src/main/java/com/pokemonbattlearena/android/fragments/BattleUIFragment.java
@@ -1,51 +1,35 @@
 package com.pokemonbattlearena.android.fragments;
 
-import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.games.Games;
-import com.google.android.gms.games.GamesActivityResultCodes;
-import com.google.android.gms.games.GamesStatusCodes;
-import com.google.android.gms.games.multiplayer.Invitation;
-import com.google.android.gms.games.multiplayer.Multiplayer;
-import com.google.android.gms.games.multiplayer.Participant;
 import com.google.android.gms.games.multiplayer.realtime.RealTimeMessage;
 import com.google.android.gms.games.multiplayer.realtime.RealTimeMessageReceivedListener;
-import com.google.android.gms.games.multiplayer.realtime.Room;
-import com.google.android.gms.games.multiplayer.realtime.RoomConfig;
-import com.google.android.gms.games.multiplayer.realtime.RoomStatusUpdateListener;
-import com.google.android.gms.games.multiplayer.realtime.RoomUpdateListener;
-import com.google.example.games.basegameutils.BaseGameUtils;
 import com.pokemonbattlearena.android.PokemonBattleApplication;
 import com.pokemonbattlearena.android.R;
 import com.pokemonbattlearena.android.TypeModel;
 import com.pokemonbattlearena.android.engine.database.Move;
 import com.pokemonbattlearena.android.engine.database.Pokemon;
 
-import java.util.ArrayList;
+import java.sql.SQLException;
 import java.util.List;
-import java.util.Random;
 
 /**
  * Created by Spencer Amann on 10/1/16.
  */
 
-public class BattleUIFragment extends Fragment implements View.OnClickListener  {
+public class BattleUIFragment extends Fragment implements View.OnClickListener, RealTimeMessageReceivedListener  {
     static final String TAG = "Pokemon Battle Room";
 
     PokemonBattleApplication mApplication = PokemonBattleApplication.getInstance();
@@ -54,9 +38,22 @@ public class BattleUIFragment extends Fragment implements View.OnClickListener  
 
     private Pokemon mPlayerPokemon;
 
+    private Pokemon mOppenentPokemon;
+
+    private List<Pokemon> mPokemonList;
+
     private List<Move> mPlayerMoves;
 
     private Button[] mMoveButtons;
+
+    private ImageView mPlayerImage;
+
+    private ImageView mOpponentImage;
+
+    private TextView mPlayerPokemonName;
+
+    private TextView mOpponentPokemonName;
+
 
 
     // launch the player selection screen
@@ -81,35 +78,18 @@ public class BattleUIFragment extends Fragment implements View.OnClickListener  
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_battleui, container, false);
+        mPokemonList = mApplication.getBattleDatabase().getPokemons();
 
-        View player1View = view.findViewById(R.id.player_1_ui);
-        View player2View = view.findViewById(R.id.player_2_ui);
+        View playerView = view.findViewById(R.id.player_1_ui);
+        View opponentView = view.findViewById(R.id.player_2_ui);
 
-        Random random = new Random();
+        mPlayerPokemonName = (TextView) playerView.findViewById(R.id.active_name_textview);
+        mOpponentPokemonName = (TextView) opponentView.findViewById(R.id.active_name_textview);
 
-        int randomPokemon = random.nextInt(150);
-        int randomPokemon2 = random.nextInt(150);
 
-        Pokemon p1 = mApplication.getBattleDatabase().getPokemons().get(randomPokemon);
-        Pokemon p2 = mApplication.getBattleDatabase().getPokemons().get(randomPokemon2);
+        mPlayerImage = (ImageView) playerView.findViewById(R.id.active_imageview);
+        mOpponentImage = (ImageView) opponentView.findViewById(R.id.active_imageview);
 
-        mPlayerPokemon = p1;
-        mPlayerMoves = mApplication.getBattleDatabase().getMovesForPokemon(p1);
-
-        mMoveButtons = new Button[mPlayerMoves.size()];
-
-        TextView p1Name = (TextView) player1View.findViewById(R.id.active_name_textview);
-        p1Name.setText(p1.getName());
-        TextView p2Name = (TextView) player2View.findViewById(R.id.active_name_textview);
-        p2Name.setText(p2.getName());
-
-        Log.d("Battle-UI", "p1: " + p1.getName() + "\tp2: " + p2.getName());
-
-        ImageView p1Image = (ImageView) player1View.findViewById(R.id.active_imageview);
-        ImageView p2Image = (ImageView) player2View.findViewById(R.id.active_imageview);
-
-        p1Image.setImageDrawable(getDrawableForPokemon(getContext(), p1.getName()));
-        p2Image.setImageDrawable(getDrawableForPokemon(getContext(), p2.getName()));
 
         setupMoveButtons(view);
 
@@ -158,10 +138,21 @@ public class BattleUIFragment extends Fragment implements View.OnClickListener  
         return c.getDrawable(id);
     }
 
-    // Sets the flag to keep this screen on. It's recommended to do that during
-    // the
-    // handshake when setting up a game, because if the screen turns off, the
-    // game will be
-    // cancelled.
+    @Override
+    public void onRealTimeMessageReceived(RealTimeMessage realTimeMessage) {
+        // Message format: pokemonName1:pokemonName2
+        Log.d(TAG, "Message Received: " + realTimeMessage.toString());
+        String[] pokemonNames = realTimeMessage.toString().split(":");
+        try {
+            mPlayerPokemon = mApplication.getBattleDatabase().getPokemonDao().queryForId(pokemonNames[0]);
+            mPlayerImage.setImageDrawable(getDrawableForPokemon(getActivity(), pokemonNames[0]));
+            mPlayerPokemonName.setText(mPlayerPokemon.getName());
 
+            mOppenentPokemon = mApplication.getBattleDatabase().getPokemonDao().queryForId(pokemonNames[1]);
+            mPlayerImage.setImageDrawable(getDrawableForPokemon(getActivity(), pokemonNames[1]));
+            mOpponentPokemonName.setText(mOppenentPokemon.getName());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
