2280933c93678df994e70d543a4366c850948a1d
Chris Cirefice <cireficc@mail.gvsu.edu>
2016-10-24 17:49:05 -0400

    Make everything inside a battle a Battle* class

--- app/src/main/java/com/pokemonbattlearena/android/engine/match/Turn.java
+++ app/src/main/java/com/pokemonbattlearena/android/engine/match/Turn.java
@@ -2,11 +2,11 @@ package com.pokemonbattlearena.android.engine.match;
 
 public class Turn {
 
-    PokemonPlayer attacker;
-    PokemonPlayer defender;
+    BattlePokemonPlayer attacker;
+    BattlePokemonPlayer defender;
     Command command;
 
-    public Turn(PokemonPlayer attacker, PokemonPlayer defender, Command command) {
+    public Turn(BattlePokemonPlayer attacker, BattlePokemonPlayer defender, Command command) {
         this.attacker = attacker;
         this.defender = defender;
         this.command = command;
