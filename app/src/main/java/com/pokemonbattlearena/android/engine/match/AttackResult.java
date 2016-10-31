7cebe3c2c1cdd44c9e1a283e58a7a3d7916c9116
Chris Cirefice <cireficc@mail.gvsu.edu>
2016-10-31 13:58:27 -0400

    Implement AttackResult and its Builder subclass
    AttackResult holds the results of an Attack command, for instance who the attacker/defender were, the move used, damage done, etc. A builder subclass eases construction of the AttackResult. AttackResult, a subclass of CommandResult, will be sent to (the) other player(s) to apply later.

---
+++ app/src/main/java/com/pokemonbattlearena/android/engine/match/AttackResult.java
@@ -0,0 +1,176 @@
+package com.pokemonbattlearena.android.engine.match;
+
+import android.util.Log;
+
+import com.pokemonbattlearena.android.engine.database.StatusEffect;
+
+public class AttackResult extends CommandResult {
+
+    private transient static final String TAG = AttackResult.class.getName();
+
+    private int moveUsedId;
+
+    private int damageDone;
+    private StatusEffect statusEffectApplied;
+    private int statusEffectTurns;
+    private boolean confused;
+    private int confusedTurns;
+    private boolean flinched;
+    private int chargingTurns;
+    private int rechargingTurns;
+    private int healingDone;
+    private int recoilTaken;
+    private boolean fainted;
+
+    private AttackResult(Builder builder) {
+
+        super();
+
+        this.targetInfo = builder.targetInfo;
+        this.moveUsedId = builder.moveUsedId;
+
+        this.damageDone = builder.damageDone;
+        this.statusEffectApplied = builder.statusEffectApplied;
+        this.statusEffectTurns = builder.statusEffectTurns;
+        this.confused = builder.confused;
+        this.confusedTurns = builder.confusedTurns;
+        this.flinched = builder.flinched;
+        this.chargingTurns = builder.chargingTurns;
+        this.rechargingTurns = builder.rechargingTurns;
+        this.healingDone = builder.healingDone;
+        this.recoilTaken = builder.recoilTaken;
+        this.fainted = builder.fainted;
+    }
+
+    public int getMoveUsedId() {
+        return moveUsedId;
+    }
+
+    public int getDamageDone() {
+        return damageDone;
+    }
+
+    public StatusEffect getStatusEffectApplied() {
+        return statusEffectApplied;
+    }
+
+    public boolean isConfused() {
+        return confused;
+    }
+
+    public int getConfusedTurns() {
+        return confusedTurns;
+    }
+
+    public boolean isFlinched() {
+        return flinched;
+    }
+
+    public int getChargingTurns() {
+        return chargingTurns;
+    }
+
+    public int getRechargingTurns() {
+        return rechargingTurns;
+    }
+
+    public int getStatusEffectTurns() {
+        return statusEffectTurns;
+    }
+
+    public int getHealingDone() {
+        return healingDone;
+    }
+
+    public int getRecoilTaken() {
+        return recoilTaken;
+    }
+
+    public boolean isFainted() {
+        return fainted;
+    }
+
+    protected static class Builder {
+
+        private TargetInfo targetInfo;
+        private int moveUsedId;
+
+        private int damageDone;
+        private StatusEffect statusEffectApplied;
+        private int statusEffectTurns;
+        private boolean confused;
+        private int confusedTurns;
+        private boolean flinched;
+        private int chargingTurns;
+        private int rechargingTurns;
+        private int healingDone;
+        private int recoilTaken;
+        private boolean fainted;
+
+        protected Builder(TargetInfo targetInfo, int moveUsedId) {
+            this.targetInfo = targetInfo;
+            this.moveUsedId = moveUsedId;
+        }
+
+        protected Builder setDamageDone(int damageDone) {
+            this.damageDone = damageDone;
+            return this;
+        }
+
+        protected Builder setStatusEffectApplied(StatusEffect statusEffect) {
+            this.statusEffectApplied = statusEffect;
+            return this;
+        }
+
+        protected Builder setStatusEffectTurns(int statusEffectTurns) {
+            this.statusEffectTurns = statusEffectTurns;
+            return this;
+        }
+
+        protected Builder setConfused(boolean confused) {
+            this.confused = confused;
+            return this;
+        }
+
+        protected Builder setConfusedTurns(int confusedTurns) {
+            this.confusedTurns = confusedTurns;
+            return this;
+        }
+
+        protected Builder setFlinched(boolean flinched) {
+            this.flinched = flinched;
+            return this;
+        }
+
+        protected Builder setChargingTurns(int chargingTurns) {
+            this.chargingTurns = chargingTurns;
+            return this;
+        }
+
+        protected Builder setRechargingTurns(int rechargingTurns) {
+            this.rechargingTurns = rechargingTurns;
+            return this;
+        }
+
+        protected Builder setHealingDone(int healingDone) {
+            this.healingDone = healingDone;
+            return this;
+        }
+
+        protected Builder setRecoilTaken(int recoilTaken) {
+            this.recoilTaken = recoilTaken;
+            return this;
+        }
+
+        protected Builder setFainted(boolean fainted) {
+            this.fainted = fainted;
+            return this;
+        }
+
+        protected AttackResult build() {
+
+            Log.i(TAG, "Building AttackResult");
+            return new AttackResult(this);
+        }
+    }
+}
