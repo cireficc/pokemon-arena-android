c14bfee8ff638555e6ee3484996f25eaa79ebc1a
Chris Cirefice <cireficc@mail.gvsu.edu>
2016-12-04 22:38:25 -0500

    Create StringBuilder log for CommandResult
    Apply logs to CommandResult so that they can be concatenated later for the entier battle log

--- app/src/main/java/com/pokemonbattlearena/android/engine/match/CommandResult.java
+++ app/src/main/java/com/pokemonbattlearena/android/engine/match/CommandResult.java
@@ -3,10 +3,21 @@ package com.pokemonbattlearena.android.engine.match;
 public class CommandResult {
 
     TargetInfo targetInfo;
+    StringBuilder logBuilder;
 
-    protected CommandResult() { }
+    protected CommandResult() {
+        this.logBuilder = new StringBuilder();
+    }
 
     public TargetInfo getTargetInfo() {
         return targetInfo;
+    }
+
+    protected void appendToLog(String s) {
+        this.logBuilder.append(s).append("\n");
+    }
+
+    public String getLog() {
+        return logBuilder.toString();
     }
 }
